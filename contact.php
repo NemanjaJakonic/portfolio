289089d1905eefe4d271431e44be3ddc4f9fb812
NemanjaJakonic <nemanjajakonic@gmail.com>
2020-03-30 13:07:26 +0200

    minor fix

--- contact.php
+++ contact.php
@@ -6,6 +6,8 @@ require 'vendor/autoload.php';
 // You need this API_KEY created on the Sendgrid website.
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+$API_KEY = getenv('API_KEY');
+
 session_start();
 $name = $email = $message = "";
 
@@ -22,7 +24,7 @@ if (isset($_POST['submit'])) {
     $email->addContent("text/plain", $message);
     // $email->addContent(
     //     "text/html", );
-    $sendgrid = new \SendGrid(getenv('API_KEY'));
+    $sendgrid = new \SendGrid(getenv($API_KEY));
 
     if ($sendgrid->send($email));
     {
