7868c8810743344f1973720841eb7a4dca36077c
NemanjaJakonic <nemanjajakonic@gmail.com>
2020-03-30 12:53:33 +0200

    added dotenv

--- contact.php
+++ contact.php
@@ -1,41 +1,35 @@
-<?php 
+<?php
 
 // You need to install the sendgrid client library so run: composer require sendgrid/sendgrid
 require 'vendor/autoload.php';
 // contains a variable called: $API_KEY that is the API Key.
 // You need this API_KEY created on the Sendgrid website.
-include_once('./credentials.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 session_start();
 $name = $email = $message = "";
 
+if (isset($_POST['submit'])) {
 
+    $name = $_POST['name'];
+    $email_id = $_POST['email'];
+    $message = $_POST['message'];
 
-if(isset($_POST['submit']))
-{
-
-  $name =$_POST['name'];
-  $email_id =$_POST['email'];
-  $message =$_POST['message'];
-
-
-
-    $email = new \SendGrid\Mail\Mail(); 
+    $email = new \SendGrid\Mail\Mail();
     $email->setFrom("nemanjajakonic@gmail.com", "$name");
-    $email->setSubject("Contact from : ".$name." <".$email_id.">");
-    $email->addTo( "nemanjajakonic@gmail.com", "Nemanja Jakonić");
+    $email->setSubject("Contact from : " . $name . " <" . $email_id . ">");
+    $email->addTo("nemanjajakonic@gmail.com", "Nemanja Jakonić");
     $email->addContent("text/plain", $message);
     // $email->addContent(
     //     "text/html", );
-    $sendgrid = new \SendGrid($API_KEY);
+    $sendgrid = new \SendGrid(getenv(API_KEY));
 
-    if($sendgrid->send($email));
+    if ($sendgrid->send($email));
     {
         $_SESSION['success'] = 'Email sent successfully!';
         header('Location: index.php#contact');
         exit;
-        
+
     }
 }
-
-?>
