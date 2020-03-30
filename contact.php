1af2dfa2f976388e8cdb4b31cdb58d46f5605d3a
NemanjaJakonic <nemanjajakonic@gmail.com>
2020-03-30 13:31:00 +0200

    minor fix

--- contact.php
+++ contact.php
@@ -32,18 +32,3 @@ if (isset($_POST['submit'])) {
 
     }
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test</title>
-</head>
-
-<body>
-    <p><?php echo $API_KEY ?></p>
-</body>
-
-</html>
